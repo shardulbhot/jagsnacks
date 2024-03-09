e03e14eebe669a79f692846a554a932554704678
shardulbhot <30757354+shardulbhot@users.noreply.github.com>
2024-03-09 19:51:26 +0530

    Add files via upload

--- token.py
+++ token.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from jose import JWTError, jwt
+import jwt
 from . import schemas
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -21,5 +21,5 @@ def verify_token(token:str,credentials_exception):
         if email is None:
             raise credentials_exception
         token_data = schemas.TokenData(email=email)
-    except JWTError:
+    except:
         raise credentials_exception
